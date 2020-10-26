a7c57c453e2431dc4fa4e4d09f06afedd25f4d31
mauriciokiubi <63929790+mauriciokiubi@users.noreply.github.com>
2020-10-26 18:42:09 -0300

    Add files via upload

--- ScriptADA.py
+++ ScriptADA.py
@@ -10,20 +10,20 @@ def TomaDatos():
 	file = open('Instancia.txt', 'r')
 	for linea in file.readlines():
 		if("NumberOfMeetings = " in linea):
-			NM = int(linea[18:21])
+			NM = int(linea[18:len(linea)])
 		elif("NumberOfAgents = " in linea):
-			NA = int(linea[17:20])
+			NA = int(linea[17:len(linea)])
 		elif("NumberOfMeetingPerAgent = " in linea):
-			NMPA = int(linea[26:30])
+			NMPA = int(linea[26:len(linea)])
 		elif("MinDisTimeBetweenMeetings = " in linea):
-			MinD = int(linea[27:30])
+			MinD = int(linea[27:len(linea)])
 		elif("MaxDisTimeBetweenMeetings = " in linea):
-			MaxD = int(linea[27:30])
+			MaxD = int(linea[27:len(linea)])
 		elif("DomainSize = " in linea):
-			DS = int(linea[13:16])	
+			DS = int(linea[13:len(linea)])	
 		elif("Agents ("+str(contador)+"):" in linea):
 			separador = " "
-			separado = linea[12:60].split(separador)
+			separado = linea[12:len(linea)].split(separador)
 			listaAux = []
 			for i in separado:
 				listaAux.append(int(i))
@@ -33,3 +33,7 @@ def TomaDatos():
 	#listaA = listaAgentes(NA, NMPA)
 	file.close()
 TomaDatos()
+string1 = "hola mundo"
+contador123 =len(string1)
+print(contador123)
+
