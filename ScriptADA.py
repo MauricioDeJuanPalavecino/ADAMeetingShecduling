b61d78efa0a5891238fdbcd1b1bc942d51044f31
mauriciokiubi <63929790+mauriciokiubi@users.noreply.github.com>
2020-10-26 16:54:51 -0300

    Add files via upload

--- ScriptADA.py
+++ ScriptADA.py
@@ -1,1 +1,24 @@
-print("hola mundo")
+def TomaDatos():
+	NM = 0
+	NA = 0
+	NMPA = 0
+	MinD = 0
+	MaxD = 0
+	DS = 0
+	contador = 0
+	file = open('Instancia.txt', 'r')
+	for linea in file.readlines():
+		if("NumberOfMeetings = " in linea):
+			NM = int(linea[18:21])
+		if("NumberOfAgents = " in linea):
+			NA = int(linea[17:20])
+		if("NumberOfMeetingPerAgent = " in linea):
+			NMPA = int(linea[26:30])
+		if("MinDisTimeBetweenMeetings = " in linea):
+			MinD = int(linea[27:30])
+		if("MaxDisTimeBetweenMeetings = " in linea):
+			MaxD = int(linea[27:30])
+		if("DomainSize = " in linea):
+			DS = int(linea[13:16])
+	file.close()
+TomaDatos()
